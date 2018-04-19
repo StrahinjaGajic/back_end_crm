d41419a575a8338afced299cabc1d3206ebf456e
Jovan Ivanovic <jovanivanovic@einutech.com>
2018-04-19 18:38:56 +0200

    Test

--- resources/views/layouts/admin/navbar.blade.php
+++ resources/views/layouts/admin/navbar.blade.php
@@ -25,12 +25,16 @@
         </div>
     </div>
     <div class="bottom">
-        <a href="" class="profile">
-            <div class="image" style="background-image: url('{{ asset('img/admin/user-profile-image.png') }}')"></div>
-        </a>
-        <a href="" class="button"><img src="{{ asset('img/admin/icon-finances-2.png') }}" \></a>
-        <a href="" class="button"><img src="{{ asset('img/admin/icon-notifications.png') }}" \></a>
-        <a href="" class="button"><img src="{{ asset('img/admin/icon-reminders.png') }}" \></a>
+        <div class="left">
+            <a href="" class="profile">
+                <div class="image" style="background-image: url('{{ asset('img/admin/user-profile-image.png') }}')"></div>
+            </a>
+        </div>
+        <div class="right">
+            <a href="" class="button"><img src="{{ asset('img/admin/icon-finances-2.png') }}" \></a>
+            <a href="" class="button"><img src="{{ asset('img/admin/icon-notifications.png') }}" \></a>
+            <a href="" class="button"><img src="{{ asset('img/admin/icon-reminders.png') }}" \></a>
+        </div>
     </div>
 </div>
 <!-- END NAVBAR -->
