1c5cdfde5152c48bfb1e9c79986b4f5d3e3b9175
Jovan Ivanovic <jovanivanovic@einutech.com>
2018-04-19 18:30:59 +0200

    Test

--- resources/views/layouts/admin/navbar.blade.php
+++ resources/views/layouts/admin/navbar.blade.php
@@ -1,18 +1,30 @@
 <!-- START NAVBAR -->
 <div class="nav-bar">
-    <div class="left">
-        <a href="javascript:void(0);" class="hamburger">
-            <img src="{{ asset('img/admin/hamburger.png') }}" />
-        </a>
-        <a href="" class="logo">
-            <img src="{{ asset('img/admin/logo-navbar.png') }}" />
-        </a>
+    <div class="top">
+        <div class="left">
+            <a href="javascript:void(0);" class="hamburger">
+                <img src="{{ asset('img/admin/hamburger.png') }}" />
+            </a>
+            <a href="" class="logo">
+                <img src="{{ asset('img/admin/logo-navbar.png') }}" />
+            </a>
+        </div>
+        <div class="right">
+            <form class="search">
+                <input type="text" placeholder="Search...">
+                <button type="submit"><img src="{{ asset('img/admin/icon-search.png') }}" /></button>
+            </form>
+            <div class="buttons">
+                <a href="" class="profile">
+                    <div class="image" style="background-image: url('{{ asset('img/admin/user-profile-image.png') }}')"></div>
+                </a>
+                <a href="" class="button"><img src="{{ asset('img/admin/icon-finances-2.png') }}" \></a>
+                <a href="" class="button"><img src="{{ asset('img/admin/icon-notifications.png') }}" \></a>
+                <a href="" class="button"><img src="{{ asset('img/admin/icon-reminders.png') }}" \></a>
+            </div>
+        </div>
     </div>
-    <div class="right">
-        <form class="search">
-            <input type="text" placeholder="Search...">
-            <button type="submit"><img src="{{ asset('img/admin/icon-search.png') }}" /></button>
-        </form>
+    <div class="bottom">
         <a href="" class="profile">
             <div class="image" style="background-image: url('{{ asset('img/admin/user-profile-image.png') }}')"></div>
         </a>
